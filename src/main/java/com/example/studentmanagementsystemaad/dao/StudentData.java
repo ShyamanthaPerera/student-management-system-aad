b1c23d2164a78468d37d7d7e2d18c86895284c52
ShyamanthaPerera <shyamanthaperera64@gmail.com>
2024-07-13 14:28:09 +0530

    create classes

--- src/main/java/com/example/studentmanagementsystemaad/dao/StudentData.java
+++ src/main/java/com/example/studentmanagementsystemaad/dao/StudentData.java
@@ -1,4 +1,13 @@
 package com.example.studentmanagementsystemaad.dao;
 
+import com.example.studentmanagementsystemaad.dto.StudentDTO;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
 public interface StudentData {
+    StudentDTO getStudent(String studentId, Connection connection) throws SQLException;
+    String saveStudent(StudentDTO studentDTO,Connection connection);
+    boolean deleteStudent(String studentId,Connection connection);
+    boolean updateStudent(String studentId,StudentDTO student,Connection connection);
 }
--- src/main/java/com/example/studentmanagementsystemaad/dao/impl/StudentDataProcess.java
+++ src/main/java/com/example/studentmanagementsystemaad/dao/impl/StudentDataProcess.java
@@ -1,11 +1,35 @@
 package com.example.studentmanagementsystemaad.dao.impl;
 
-import java.sql.Connection;
+import com.example.studentmanagementsystemaad.dao.StudentData;
+import com.example.studentmanagementsystemaad.dto.StudentDTO;
 
-public class StudentDataProcess {
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class StudentDataProcess implements StudentData {
 
     static String SAVE_STUDENT = "INSERT INTO students (id, name, email, city, level) VALUES(?,?,?,?,?)";
     static String GET_STUDENT = "SELECT * FROM students WHERE id=?";
     static String UPDATE_STUDENT = "UPDATE student SET name = ?, city = ?, email = ?, level = ? WHERE id = ?";
     static String DELETE_STUDENT = "DELETE FROM student WHERE id=?";
+
+    @Override
+    public StudentDTO getStudent(String studentId, Connection connection) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public String saveStudent(StudentDTO studentDTO, Connection connection) {
+        return null;
+    }
+
+    @Override
+    public boolean deleteStudent(String studentId, Connection connection) {
+        return false;
+    }
+
+    @Override
+    public boolean updateStudent(String studentId, StudentDTO student, Connection connection) {
+        return false;
+    }
 }
